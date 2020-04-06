7c3a9599df3605d9fa26a6de2dbf0f6063a8cf78
MHMoritz3 <mhmoritz3@outlook.com>
2020-04-06 16:08:41 +0200

    Beginning Workg on LibTiff

--- conanfile.py
+++ conanfile.py
@@ -1,62 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from conans import ConanFile, CMake, tools
 import os
+import shutil
 
 
-class LibnameConan(ConanFile):
-    name = "libname"
-    description = "Keep it short"
-    topics = ("conan", "libname", "logging")
-    url = "https://github.com/bincrafters/conan-libname"
-    homepage = "https://github.com/original_author/original_lib"
-    license = "MIT"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
+class LibtiffConan(ConanFile):
+    name = "libtiff"
+    description = "Library for Tag Image File Format (TIFF)"
+    version = "4.0.9"
+    url = "http://github.com/bincrafters/conan-tiff"
+    author = "Bincrafters <bincrafters@gmail.com>"
+    license = "MIT"
+    homepage = "http://www.simplesystems.org/libtiff"
+    exports = ["LICENSE.md"]
     exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
-
-    settings = "os", "arch", "compiler", "build_type"
+    settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
+    default_options = {'shared': False, 'fPIC': True}
+    requires = "zlib/1.2.11"
 
     _source_subfolder = "source_subfolder"
-    _build_subfolder = "build_subfolder"
-    _cmake = None
-
-    requires = (
-        "zlib/1.2.11"
-    )
 
     def config_options(self):
-        if self.settings.os == 'Windows':
-            del self.options.fPIC
+        if self.settings.os == "Windows":
+            self.options.remove("fPIC")
+        del self.settings.compiler.libcxx
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
-
-    def _configure_cmake(self):
-        if not self._cmake:
-            self._cmake = CMake(self)
-            self._cmake.definitions["BUILD_TESTS"] = False  # example
-            self._cmake.configure(build_folder=self._build_subfolder)
-        return self._cmake
+        tools.get("http://download.osgeo.org/libtiff/tiff-{0}.zip".format(self.version))
+        os.rename('tiff-' + self.version, self._source_subfolder)
+        os.rename(os.path.join(self._source_subfolder, "CMakeLists.txt"),
+                  os.path.join(self._source_subfolder, "CMakeListsOriginal.txt"))
+        shutil.copy("CMakeLists.txt",
+                    os.path.join(self._source_subfolder, "CMakeLists.txt"))
 
     def build(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+
+        cmake.definitions['CMAKE_INSTALL_LIBDIR'] = 'lib'
+        cmake.definitions['CMAKE_INSTALL_BINDIR'] = 'bin'
+        cmake.definitions['CMAKE_INSTALL_INCLUDEDIR'] = 'include'
+
+        cmake.definitions["lzma"] = False
+        cmake.definitions["jpeg"] = False
+        cmake.definitions["jbig"] = False
+        if self.options.shared and self.settings.compiler == "Visual Studio":
+            # https://github.com/Microsoft/vcpkg/blob/master/ports/tiff/fix-cxx-shared-libs.patch
+            tools.replace_in_file(os.path.join(self._source_subfolder, 'libtiff', 'CMakeLists.txt'),
+                                  r'set_target_properties(tiffxx PROPERTIES SOVERSION ${SO_COMPATVERSION})',
+                                  r'set_target_properties(tiffxx PROPERTIES SOVERSION ${SO_COMPATVERSION} '
+                                  r'WINDOWS_EXPORT_ALL_SYMBOLS ON)')
+
+        if self.settings.os == "Windows" and self.settings.compiler != "Visual Studio":
+            tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeListsOriginal.txt"),
+                                      "find_library(M_LIBRARY m)",
+                                      "if (NOT MINGW)\n  find_library(M_LIBRARY m)\nendif()")
+            if self.version == '4.0.8':
+                # only one occurence must be patched. fixed in 4.0.9
+                tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeListsOriginal.txt"),
+                                      "if (UNIX)",
+                                      "if (UNIX OR MINGW)")
+
+        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeListsOriginal.txt"),
+            "add_subdirectory(tools)\nadd_subdirectory(test)\nadd_subdirectory(contrib)\nadd_subdirectory(build)\n"
+            "add_subdirectory(man)\nadd_subdirectory(html)", "")
+
+        cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
+        cmake.configure(source_folder=self._source_subfolder)
         cmake.build()
+        cmake.install()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
-        cmake.install()
-        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
-        # If so, you can just remove the lines below
-        include_folder = os.path.join(self._source_subfolder, "include")
-        self.copy(pattern="*", dst="include", src=include_folder)
-        self.copy(pattern="*.dll", dst="bin", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+        self.copy("COPYRIGHT", src=self._source_subfolder, dst="licenses", ignore_case=True, keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs = ["tiff", "tiffxx"]
+        if self.settings.os == "Windows" and self.settings.build_type == "Debug" and self.settings.compiler == 'Visual Studio':
+            self.cpp_info.libs = [lib+'d' for lib in self.cpp_info.libs]
+        if self.options.shared and self.settings.os == "Windows" and self.settings.compiler != 'Visual Studio':
+            self.cpp_info.libs = [lib+'.dll' for lib in self.cpp_info.libs]
+        if self.settings.os == "Linux":
+            self.cpp_info.libs.append("m")
